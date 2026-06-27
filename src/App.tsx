6bb4904d79e8d89e4579538603d3b6244ae59329
ARUNKUMAR M <arunkumar.m@vealthx.com>
2026-06-27 18:04:44 +0530

    Update API endpoint for payment completion

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
     try {
     
       const response = await axios.post(
-        `https://vealthx-ollamavm2.centralindia.cloudapp.azure.com/zoho-subscription-test/api/hostedpage/payment-complete`,
+        `https://vealthx-ollamavm2.centralindia.cloudapp.azure.com/zoho-subscription-test/api/v2/hostedpage/payment-complete`,
         { hostedpage_id: paymentDetails.hostedpage_id }
       );
 
@@ -70,7 +70,7 @@ function App() {
         try {
           // Send only hostedpage_id to the API
         const response = await axios.post(
-        `https://vealthx-ollamavm2.centralindia.cloudapp.azure.com/zoho-subscription-test/api/hostedpage/payment-complete`,
+        `https://vealthx-ollamavm2.centralindia.cloudapp.azure.com/zoho-subscription-test/api/v2/hostedpage/payment-complete`,
        { hostedpage_id: paymentDetails.hostedpage_id }
       );
 
